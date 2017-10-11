7e74ae74625234fc90d91147679863301089c6bc
Kamal Mango <kamal.mango7@gmail.com>
2017-10-11 18:56:35 +0200

    add deck logic

---
+++ components/atoms/DeckCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class DeckCard extends Component {
+  render () {
+    return (
+
+    )
+  }
+}
+
+export default DeckCard
--- components/organisms/Decks.js
+++ components/organisms/Decks.js
@@ -3,22 +3,23 @@ import { View, Text } from 'react-native'
 import { connect } from 'react-redux'
 import { receiveDecks } from '../../actions' 
 import { getDecks } from '../../utils/api'
+import DeckList from '../molecules/DeckList'
 
 class Decks extends Component {
   componentDidMount() {
     getDecks().then(decks => this.props.receiveDecks(JSON.parse(decks)))
   }
   render() {
-    console.warn('decks ', this.props.decks)
+    // console.warn('decks ', this.props.decks)
     return (
       <View>
-        <Text>{JSON.stringify(this.props.decks)}</Text>
+        <DeckList decks={this.props.decks} />
       </View>
     )
   }
 }
 
-function mapStateToProps ({ decks }) {
+function mapStateToProps (decks) {
   return {
     decks
   }
